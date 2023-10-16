44d01f81e6d05007b3651afea8fb1088604095c1
ABDUR-RAZZAK-ROUZEX <abdurrazzak1663@gmail.com>
2023-10-16 13:29:41 +0600

    post create component and post creation is success

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PostCreateComponent from "./components/post-create/post-create.component";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreateComponent />
+    </div>
+  );
 };
 
 export default App;
